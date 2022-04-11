3f35c39c6647c2a81fe964af7c319a7f8a567405
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-04-10 22:34:26 -0500

    added some styling to search bar and images

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SplashPage.css';
 
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedIn } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 import logoImg from '../../images/splash/logo.png';
+import mainBlob from '../../images/splash/bigBlob.png';
+import smallBlob from '../../images/splash/blob2.png';
 
 const SplashPage = () => {
   return (
@@ -41,19 +43,35 @@ const SplashPage = () => {
         <div className='splash-container'>
           <div className='splash-content'>
             <div className='content-details'>
-              <h1 className='title'>
-                Let's Explore The Unknown
-              </h1>
+              <h1 className='title'>Let's Explore The Unknown</h1>
               <p className='splash-description'>
                 Lollipop I love croissant croissant I love pudding cheesecake. I love lemon drops jelly apple pie I love toffee candy sweet roll chupa chups.
               </p>
 
               <div className='search'>
-                <input className='search-bar' type='text' placeholder="Dive into possibilities..." required />
+                <input className='search-bar' type='text' placeholder="Your Email Address" required />
                 <input className='search-btn' type='button' value="Join" />
               </div>
 
+              <div className='sm-list'>
+                <a className='sm-link' href="https://www.google.com/">
+                  <FaTwitter />
+                </a>
+                <a className='sm-link' href="https://www.google.com/">
+                  <FaInstagram />
+                </a>
+                <a className='sm-link' href="https://www.google.com/">
+                  <FaFacebookF />
+                </a>
+                <a className='sm-link' href="https://www.google.com/">
+                  <FaLinkedinIn />
+                </a>
+              </div>
+            </div>
 
+            <div className='image-group'>
+              <img className='blob-large' alt='large blob' src={mainBlob} />
+              <img className='blob-large' alt='large blob' src={smallBlob} />
             </div>
           </div>
         </div>
