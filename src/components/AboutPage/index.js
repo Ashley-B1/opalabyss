cd97a6280394d17d774300b9daa3f1f30cad2bd9
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-04-11 22:31:34 -0500

    added content to about page

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import './AboutPage.css'
+import './AboutPage.css';
+
+import NavBar from '../NavBar';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <>
+      <NavBar />
+      <div className='about-container'>
+        <div className='inner-section'>
+          <h1 className='about-title'>About Us</h1>
+          <p className='about-content'>
+            Lemon drops sesame snaps jelly beans toffee cookie. Cotton candy tiramisu dragée I love sesame snaps. Halvah jujubes danish pastry cake. Gummies cheesecake cookie I love pastry I love. I love muffin jujubes cake croissant lollipop chocolate cake lollipop. Marshmallow carrot cake sweet cotton candy tart cookie chocolate bar dessert. Oat cake fruitcake chocolate apple pie cake sesame snaps toffee sweet roll sweet. Jujubes cake sweet topping carrot cake jelly beans I love liquorice.
+          </p>
+          <div className='info'>
+            <button className='about-button'>
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
